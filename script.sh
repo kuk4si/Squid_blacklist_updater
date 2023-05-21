a7fc3cccc05d8cf700e5251856b24ec3ced5eab0
Andrey <94981303+kuk4si@users.noreply.github.com>
2023-05-21 19:05:32 +0200

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+wget=/usr/bin/wget
+tar=/usr/bin/tar
+
+OS="Ubuntu"
+LOCATION="/var/squidGuard"
+WGET_FILE_OUTPUT_NAME="squid_blacklist.tar"
+DB_URL="http://dsi.ut-capitole.fr/blacklists/download/blacklists_for_pfsense.tar.gz"
+FILENAME_DURING_UNTARRING="blacklists"
+CUSTOM_BLACKLISTS=""
+LOGDIRECTORY="/var/log/squidDBupdater"
+LOGFILE="${LOGDIRECTORY}/logs"
+
+
+
+#----------   Script   -------------
+
+
+
+sudo mkdir $LOGDIRECTORY
+touch $LOGFILE
+sudo rm -r $LOCATION/$FILENAME_DURING_UNTARRING
+sudo $wget -O $LOCATION/$WGET_FILE_OUTPUT_NAME $DB_URL
+
+if [ -z "$FILENAME_DURING_UNTARRING" ]
+then
+	echo "[$(date)] \$FILENAME_DURING_UNTARRING is empty." >> $LOGFILE
+	sudo tar xzf $LOCATION/$WGET_FILE_OUTPUT_NAME -C $LOCATION/
+else
+	echo "[$(date)] Renaming tar output to '$FILENAME_DURING_UNTARRING'" >> $LOGFILE
+	sudo mkdir $LOCATION/$FILENAME_DURING_UNTARRING && sudo tar xzf $LOCATION/$WGET_FILE_OUTPUT_NAME -C $LOCATION/$FILENAME_DURING_UNTARRING --strip-components 1
+fi
+
+sudo rm $LOCATION/$WGET_FILE_OUTPUT_NAME
+
+if [ -z "$CUSTOM_BLACKLISTS" ]
+then
+	echo "[$(date)] Custom blacklists not found." >> $LOGFILE
+else
+	echo "[$(date)] Copy additional lists: $CUSTOM_BLACKLISTS" >> $LOGFILE
+	sudo cp -R $CUSTOM_BLACKLISTS $LOCATION/$FILENAME_DURING_UNTARRING/
+fi
+
+
+#Restart Squid Service
+
+if [[ $OS -eq "Ubuntu" ]]
+then
+	echo "[$(date)] Restarting Ubuntu Squid Service" >> $LOGFILE
+	sudo systemctl restart squid
+	echo "[$(date)] Done" >> $LOGFILE
+elif [[ $OS -eq "Gentoo" ]]
+then
+	echo "[$(date)] Restarting Gentoo Squid Service" >> $LOGFILE
+        sudo rc-service squid restart
+        echo "[$(date)] Done" >> $LOGFILE
+fi
+
